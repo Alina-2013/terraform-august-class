3772e148ac427b949c7eaf6d1e46b562be42a035
farrukh90 <farrukhsadykov@gmail.com>
2021-11-03 23:34:19 +0000

    added 3 rules

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -17,6 +17,20 @@ resource "aws_security_group" "allow_tls" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
