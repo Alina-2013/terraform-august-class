88d88627704115ed2d0d077e3f76cb02c9555c36
farrukh90 <farrukhsadykov@gmail.com>
2021-11-03 23:32:34 +0000

    added fmt

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -1,36 +1,28 @@
 resource "aws_key_pair" "class" {
-	key_name = "class-key"
-	public_key = file("~/.ssh/id_rsa.pub")
-    tags = {
-        Name = "HelloWorld"
-        Env = "Dev"
-        Team = "DevOps"
-    }
+  key_name   = "class-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+  tags = {
+    Name = "HelloWorld"
+    Env  = "Dev"
+    Team = "DevOps"
+  }
 }
-
-resource "aws_security_group" "class-sec-group" {
-  name        = "class-sec-group"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  ingress = [
-    {
-      description      = "TLS from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
-
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  ]
-
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "allow_tls"
   }
@@ -38,12 +30,12 @@ resource "aws_security_group" "class-sec-group" {
 
 
 resource "aws_instance" "web" {
-	ami = "ami-01cc34ab2709337aa"
-	instance_type = "t2.micro"
-    key_name = aws_key_pair.class.key_name
-    tags = {
-        Name = "HelloWorld"
-        Env = "Dev"
-        Team = "DevOps"
-    }
+  ami           = "ami-01cc34ab2709337aa"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.class.key_name
+  tags = {
+    Name = "HelloWorld"
+    Env  = "Dev"
+    Team = "DevOps"
+  }
 }
