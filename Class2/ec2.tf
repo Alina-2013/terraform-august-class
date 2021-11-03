c48864a5faac8c213102776d0f152a9f7c299ffd
farrukh90 <farrukhsadykov@gmail.com>
2021-11-03 23:26:10 +0000

    added tags

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -1,6 +1,11 @@
 resource "aws_key_pair" "class" {
 	key_name = "class-key"
 	public_key = file("~/.ssh/id_rsa.pub")
+    tags = {
+        Name = "HelloWorld"
+        Env = "Dev"
+        Team = "DevOps"
+    }
 }
 
 
@@ -8,4 +13,9 @@ resource "aws_instance" "web" {
 	ami = "ami-01cc34ab2709337aa"
 	instance_type = "t2.micro"
     key_name = aws_key_pair.class.key_name
+    tags = {
+        Name = "HelloWorld"
+        Env = "Dev"
+        Team = "DevOps"
+    }
 }
