5e32bd1dcca205c78d6715a47a1279a3ad62e6e9
farrukh90 <farrukhsadykov@gmail.com>
2021-11-03 23:10:54 +0000

    added ec2

---
+++ Class2/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_key_pair" "class" {
+	key_name = "class-key"
+	public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+resource "aws_instance" "web" {
+	ami = "ami-01cc34ab2709337aa"
+	instance_type = "t2.micro"
+}
