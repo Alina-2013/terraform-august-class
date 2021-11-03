32468f09312376782cbe4b5c1ef04f299ff4c009
farrukh90 <farrukhsadykov@gmail.com>
2021-11-03 23:31:29 +0000

    added sec group

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -8,6 +8,34 @@ resource "aws_key_pair" "class" {
     }
 }
 
+resource "aws_security_group" "class-sec-group" {
+  name        = "class-sec-group"
+  description = "Allow TLS inbound traffic"
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
 
 resource "aws_instance" "web" {
 	ami = "ami-01cc34ab2709337aa"
