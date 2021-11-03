28d452750220e0c4cec68f34e159482fb7482a3a
farrukh90 <farrukhsadykov@gmail.com>
2021-11-03 23:38:41 +0000

    added interpolation for sec group

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -46,6 +46,7 @@ resource "aws_security_group" "allow_tls" {
 resource "aws_instance" "web" {
   ami           = "ami-01cc34ab2709337aa"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name      = aws_key_pair.class.key_name
   tags = {
     Name = "HelloWorld"
