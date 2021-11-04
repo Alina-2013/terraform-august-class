116a507215b167f0deaf577da64c2a440e8bdd19
farrukh90 <farrukhsadykov@gmail.com>
2021-11-04 00:22:57 +0000

    added output to sep file

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -70,33 +70,3 @@ resource "aws_instance" "web" {
   user_data  = file("userdata.sh")
   tags = local.common_tags
 }
-
-output "AMI" {
-  value = aws_instance.web.ami
-}
-
-output "ID" {
-  value = aws_instance.web.id
-}
-
-
-output "ARN" {
-  value = aws_instance.web.arn
-}
-
-output "PIP" {
-  value = aws_instance.web.public_ip
-}
-
-output "key_name" {
-  value = aws_instance.web.key_name
-}
-
-output "private_ip" {
-  value = aws_instance.web.private_ip
-}
-
-
-output "KEY_PAIR_INFO" {
-  value = "${aws_key_pair.class.key_name} ${aws_key_pair.class.id} ${aws_key_pair.class.arn}" 
-}
