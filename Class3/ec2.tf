563a1fd073ef7475b2427bb8b5e2055fd4afde34
farrukh90 <farrukhsadykov@gmail.com>
2021-11-06 15:43:30 +0000

    added sec group and key

--- Class3/ec2.tf
+++ Class3/ec2.tf
@@ -1,7 +1,48 @@
+
+resource "aws_key_pair" "class" {
+  key_name   = "class2-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "web" {
-  ami               = data.aws_ami.centos.id
-  instance_type     = "t3.micro"
-  availability_zone = data.aws_availability_zones.all.names[0]
+  ami                    = data.aws_ami.centos.id
+  instance_type          = "t3.micro"
+  availability_zone      = data.aws_availability_zones.all.names[0]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name               = aws_key_pair.class.key_name
+
   provisioner "remote-exec" {
     connection {
       host        = aws_instance.web.public_ip
