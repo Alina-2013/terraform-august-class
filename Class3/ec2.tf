25f5ef9ed4bb36a33808aba77668d6db07b934fc
farrukh90 <farrukhsadykov@gmail.com>
2021-11-06 16:14:02 +0000

    added null

--- Class3/ec2.tf
+++ Class3/ec2.tf
@@ -42,7 +42,25 @@ resource "aws_instance" "web" {
   availability_zone      = data.aws_availability_zones.all.names[0]
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = aws_key_pair.class.key_name
+}
 
+resource "null_resource" "commands" {
+  depends_on = ["aws_instance.web"]
+  triggers = {
+    always_run = timestamp()
+  }
+  # Push files to remote server
+  provisioner "file" {
+    connection {
+      host        = aws_instance.web.public_ip
+      type        = "ssh"
+      user        = "centos"
+      private_key = file("~/.ssh/id_rsa")
+    }
+    source      = "r1soft.repo"
+    destination = "/tmp/r1soft.repo"
+  }
+  # Execute linux commands on remote machine
   provisioner "remote-exec" {
     connection {
       host        = aws_instance.web.public_ip
@@ -55,6 +73,7 @@ resource "aws_instance" "web" {
       "sudo yum install httpd -y",
       "sudo systemctl start httpd",
       "sudo systemctl enable httpd",
+      "sudo cp /tmp/r1soft.repo /etc/yum.repos.d/r1soft.repo",
       "sudo yum install telnet -y"
     ]
   }
