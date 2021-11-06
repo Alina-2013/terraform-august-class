04f9f1a5d37f3033793dba576292b4e63b88bf1b
farrukh90 <farrukhsadykov@gmail.com>
2021-11-06 16:36:28 +0000

    added   instance_type          = var.instance_type

--- Class3/ec2.tf
+++ Class3/ec2.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.centos.id
-  instance_type          = "t3.micro"
+  instance_type          = var.instance_type
   availability_zone      = data.aws_availability_zones.all.names[0]
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = aws_key_pair.class.key_name
@@ -50,7 +50,7 @@ resource "aws_instance" "web" {
 
 resource "aws_instance" "web2" {
   ami                    = data.aws_ami.centos.id
-  instance_type          = "t3.micro"
+  instance_type          = var.instance_type
   availability_zone      = data.aws_availability_zones.all.names[0]
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = aws_key_pair.class.key_name
