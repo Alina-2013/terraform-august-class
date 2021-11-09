ff8023f31a616054c03de4a2eb4fb6ae8672b72b
farrukh90 <farrukhsadykov@gmail.com>
2021-11-09 00:32:34 +0000

    added nat

--- Class4/private_subnet.tf
+++ Class4/private_subnet.tf
@@ -14,5 +14,14 @@ resource "aws_subnet" "subnet3" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = data.aws_availability_zones.available.names[2]
+}
 
+
+resource "aws_eip" "example" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.example.id
+  subnet_id     = aws_subnet.public_subnet1.id
 }
