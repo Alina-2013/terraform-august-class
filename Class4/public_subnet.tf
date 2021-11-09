6a26eeaa7f2f4e7d3793e89fc39c4d1f6589140f
farrukh90 <farrukhsadykov@gmail.com>
2021-11-09 00:45:30 +0000

    fixed naming

--- Class4/public_subnet.tf
+++ Class4/public_subnet.tf
@@ -21,17 +21,17 @@ resource "aws_internet_gateway" "gw" {
 }
 
 
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.subnet1.id
+resource "aws_route_table_association" "public_subnet1" {
+  subnet_id      = aws_subnet.public_subnet1.id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.subnet2.id
+resource "aws_route_table_association" "public_subnet2" {
+  subnet_id      = aws_subnet.public_subnet2.id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public3" {
-  subnet_id      = aws_subnet.subnet3.id
+resource "aws_route_table_association" "public_subnet3" {
+  subnet_id      = aws_subnet.public_subnet3.id
   route_table_id = aws_route_table.public.id
 }
