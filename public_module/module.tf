eec75f7b6cb371d9060cc25771c633af346f3e32
farrukh90 <farrukhsadykov@gmail.com>
2021-11-09 01:42:11 +0000

    added more files

--- public_module/module.tf
+++ public_module/module.tf
@@ -1,15 +1,21 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  name = "dev"
-  cidr = "10.0.0.0/16"
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-//   enable_nat_gateway = true
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  name   = var.vpc_name
+  cidr   = var.vpc_cidr
+  azs = [
+    data.aws_availability_zones.available.names[0],
+    data.aws_availability_zones.available.names[1],
+    data.aws_availability_zones.available.names[2],
+  ]
+  private_subnets = [
+    var.private_subnet1,
+    var.private_subnet2,
+    var.private_subnet3,
+  ]
+  public_subnets = [
+    var.public_subnet1,
+    var.public_subnet2,
+    var.public_subnet3,
+  ]
+  tags = var.tags
 }
