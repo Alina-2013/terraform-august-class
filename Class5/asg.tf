b0419369c0efc28b0032b2b24ca68d97b72d74b5
farrukh90 <farrukhsadykov@gmail.com>
2021-11-11 01:09:56 +0000

    added encoded userdata

--- Class5/asg.tf
+++ Class5/asg.tf
@@ -40,5 +40,6 @@ module "wordpress-asg" {
   instance_type     = "t3.micro"
   ebs_optimized     = true
   enable_monitoring = true
+  user_data_base64	= "IyEvYmluL2Jhc2ggIApzdWRvIHl1bSBpbnN0YWxsIGh0dHBkIHBocCBwaHAtbXlzcWwgd2dldCAgLXkgCnN1ZG8gc3lzdGVtY3RsIHJlc3RhcnQgaHR0cGQgCnN1ZG8gc3lzdGVtY3RsIGVuYWJsZSBodHRwZCAKd2dldCAgaHR0cHM6Ly93b3JkcHJlc3Mub3JnL3dvcmRwcmVzcy00LjAuMzIudGFyLmd6CnN1ZG8gdGFyIC14ZiB3b3JkcHJlc3MtNC4wLjMyLnRhci5neiAtQyAvdmFyL3d3dy9odG1sLyAKc3VkbyBtdiAvdmFyL3d3dy9odG1sL3dvcmRwcmVzcy8qIC92YXIvd3d3L2h0bWwvIApzdWRvIHN5c3RlbWN0bCByZXN0YXJ0IGh0dHBkIApzdWRvIGNob3duIC1SIGFwYWNoZTphcGFjaGUgL3Zhci93d3cvaHRtbC8Kc3VkbyBzeXN0ZW1jdGwgcmVzdGFydCBodHRwZAo="
   tags_as_map = var.tags
 }
