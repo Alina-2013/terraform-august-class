cc9bc2c1864842c97d6be28476dcb0f5c9074702
farrukh90 <farrukhsadykov@gmail.com>
2021-11-11 00:28:07 +0000

    added more rules

--- Class5/output.tf
+++ Class5/output.tf
@@ -1,15 +1,15 @@
-output vpc_id {
-    value = module.vpc.vpc_id
+output "vpc_id" {
+  value = module.vpc.vpc_id
 }
 
-output private_subnets {
-    value = module.vpc.private_subnets
+output "private_subnets" {
+  value = module.vpc.private_subnets
 }
 
-output public_subnets {
-    value = module.vpc.public_subnets
+output "public_subnets" {
+  value = module.vpc.public_subnets
 }
 
-output ami_id {
-    value = data.aws_ami.ubuntu.id
+output "ami_id" {
+  value = data.aws_ami.ubuntu.id
 }
