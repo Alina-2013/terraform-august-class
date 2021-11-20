6e4178307a931cb192c0e9d8e59370eef2fb7e8d
farrukh90 <farrukhsadykov@gmail.com>
2021-11-20 18:08:59 +0000

    added more

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -64,7 +64,9 @@ resource "azuread_user" "bob" {
 
 resource "azuread_group" "example" {
 	display_name = "A-AD-Group"
-    mail_enabled = false
+    mail_enabled     = true
+    mail_nickname    = "ExampleGroup"
+    security_enabled = true
 	members = [
 	azuread_user.example.object_id,
 	/* more users */
