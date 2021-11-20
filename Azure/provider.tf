3841b07dca4e61d91f2ac3ceb6b99351abdf03c2
farrukh90 <farrukhsadykov@gmail.com>
2021-11-20 18:09:49 +0000

    types            = ["Unified"]

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -67,6 +67,8 @@ resource "azuread_group" "example" {
     mail_enabled     = true
     mail_nickname    = "ExampleGroup"
     security_enabled = true
+    types            = ["Unified"]
+
 	members = [
 	azuread_user.example.object_id,
 	/* more users */
