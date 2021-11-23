2228c56f4ff9f178e941ed70f574029a26b9d5af
farrukh90 <farrukhsadykov@gmail.com>
2021-11-23 01:14:11 +0000

    added mysql

--- GCP/vm.tf
+++ GCP/vm.tf
@@ -22,7 +22,7 @@ resource "google_compute_firewall" "allow-http" {
 	network = "default"
 	allow {
 		protocol = "tcp"
-		ports = ["80","22"]
+		ports = ["80","22","3306"]
 	}
 	source_tags = [var.vm_config["network_tags"]]
 }
